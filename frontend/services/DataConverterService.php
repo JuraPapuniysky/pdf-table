55ecdce929b50f1338e6fa40620238f86b500032
jura <jurapapunitsky@gmail.com>
2019-06-13 10:36:22 +0300

    fill DataModel

--- frontend/services/DataConverterService.php
+++ frontend/services/DataConverterService.php
@@ -11,6 +11,69 @@ class DataConverterService
     public function getDataModels(string $text): array
     {
         $arrayText = explode(PHP_EOL, $text);
+
+        $data = $this->getDatesPOPNRS($arrayText);
+
+        $convertedData = [];
+        foreach ($data[2] as $key => $item) {
+            $arrayData = explode(' ', explode('Сторінка', $item)[0]);
+            $dataModel = new Data();
+            $dataModel->time = $data[0][$key];
+            $dataModel->serviceType = $data[1][$key];
+            if (count($arrayData) === 12) {
+                $dataModel->direct = $this->getDirect($arrayData);
+                $dataModel->countTime = $arrayData[2];
+                $dataModel->phone = $arrayData[3];
+                $dataModel->extraMinutes = $arrayData[4];
+                $dataModel->cost = $arrayData[5];
+                $dataModel->internationalRoamingCost = $arrayData[6];
+                $dataModel->nationalRoamingCost = $arrayData[7];
+                $dataModel->tariffZone = str_replace(PHP_EOL, ' ', $arrayData[8]);
+                $dataModel->tariffTime = $this->getTariffTime($arrayData);
+
+                array_push($convertedData, $dataModel);
+            }
+
+            if (count($arrayData) === 11) {
+                $dataModel->direct = $this->getDirect($arrayData);
+                $dataModel->countTime = $arrayData[2];
+                $dataModel->phone = $arrayData[3];
+                $dataModel->extraMinutes = '';
+                $dataModel->cost = $arrayData[4];
+                $dataModel->internationalRoamingCost = $arrayData[5];
+                $dataModel->nationalRoamingCost = $arrayData[6];
+                $dataModel->tariffZone = str_replace(PHP_EOL, ' ', $arrayData[7]);
+                $dataModel->tariffTime = $arrayData[8] . ' ' . str_replace(PHP_EOL, ' ', $arrayData[9]) . $arrayData[10];
+
+                array_push($convertedData, $dataModel);
+            }
+
+            if (count($arrayData) === 9) {
+                $dataModel->direct = $this->getDirect($arrayData);
+                $dataModel->countTime = $arrayData[2];
+                $dataModel->phone = $arrayData[3];
+                $dataModel->extraMinutes = '';
+                $dataModel->cost = $arrayData[4];
+                $dataModel->internationalRoamingCost = $arrayData[5];
+                $dataModel->nationalRoamingCost = $arrayData[6];
+                $dataModel->tariffZone = '';
+                $dataModel->tariffTime = $arrayData[7] . ' ' . $arrayData[8];
+
+                array_push($convertedData, $dataModel);
+            }
+        }
+
+        return $convertedData;
+        //return [$dates, $popNrs, $data];
+    }
+
+    public function createXlsFromDataModels(array $dataModels): array
+    {
+        return $dataModels;
+    }
+
+    private function getDatesPOPNRS(array $arrayText): array
+    {
         $dates = [];
         $popNrs = [];
         $data = [];
@@ -20,7 +83,8 @@ class DataConverterService
             }
 
             if (strstr(trim($arrayText[$i]), 'POP') !== false) {
-                array_push($popNrs, $arrayText[$i] . $arrayText[$i + 1] . $arrayText[$i + 2] . $arrayText[$i + 3] . $arrayText[$i + 4]);
+                array_push($popNrs,
+                    $arrayText[$i] . $arrayText[$i + 1] . $arrayText[$i + 2] . $arrayText[$i + 3] . $arrayText[$i + 4]);
             }
 
             if (strstr(trim($arrayText[$i]), 'Вхідне') !== false) {
@@ -39,8 +103,13 @@ class DataConverterService
         return [$dates, $popNrs, $data];
     }
 
-    public function createXlsFromDataModels(array $dataModels): array
+    private function getDirect(array $item): string
     {
-        return $dataModels;
+        return $item[0] . ' ' . $item[1];
+    }
+
+    private function getTariffTime(array $item): string
+    {
+        return $item[9] . ' ' . str_replace(PHP_EOL, ' ', $item[10]) . $item[11];
     }
 }
